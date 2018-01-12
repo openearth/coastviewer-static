79876ef05190d4b881f0f65b5496f4fdb838ce9b
CindyvdVries <cindyvdvries@gmail.com>
2018-01-12 16:57:08 +0100

    Toggle layers
    Added a toggle functionality for the layers. In the datalayers.json the
    different layers that need to be added can be stored. Layers can be
    added individually or in layergroups. Add 'type': 'group to the json.

--- src/components/data-layers.js
+++ src/components/data-layers.js
@@ -1,6 +1,14 @@
-import {experimental, GeoJsonLayer} from 'deck.gl';
-import {bus} from '@/event-bus.js';
-const {DeckGLJS, MapControllerJS} = experimental;
+import {
+  experimental,
+  GeoJsonLayer
+} from 'deck.gl';
+import {
+  bus
+} from '@/event-bus.js';
+const {
+  DeckGLJS,
+  MapControllerJS
+} = experimental;
 import Vue2MapboxGL from 'vue2mapbox-gl';
 import Vue from 'vue';
 import mapboxgl from 'mapbox-gl';
@@ -10,85 +18,12 @@ Vue.use(Vue2MapboxGL);
 
 export default {
   name: 'DataLayers',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App',
-      layers: [
-        {
-          "id": "line-NAP_-20m",
-          "type": "line",
-          "source": {
-            "url": "mapbox://giosans.636mnlg8",
-            "type": "vector"
-          },
-          "source-layer": "Doorgaande_NAP_-20_m_lijn",
-          "minzoom": 4,
-          "paint": {
-            "line-color": "blue",
-            "line-width": 3
-          }
-        },
-        {
-          "id": "label-NAP_-20m",
-          "type": "symbol",
-          "source": {
-            "url": "mapbox://giosans.636mnlg8",
-            "type": "vector"
-          },
-          "source-layer": "Doorgaande_NAP_-20_m_lijn",
-          "minzoom": 4,
-          "layout": {
-            "text-field": "{Name}",
-            "symbol-placement": "line",
-            "text-size": 16
-          },
-          "paint": {
-            "text-color": "white",
-            "text-halo-color": "black",
-            "text-halo-width": 2,
-            "text-halo-blur": 1
-          },
-
-        },
-        {
-          "id": "line_NAP_-20m_2km",
-          "type": "line",
-          "source": {
-            "url": "mapbox://giosans.636mnlg8",
-            "type": "vector"
-          },
-          "source-layer": "Doorgaande_NAP_-20_m_lijn__2_km",
-          "minzoom": 4,
-          "paint": {
-            "line-color": "white",
-            "line-width": 3
-          }
-        },
-        {
-          "id": "label_NAP_-20m_2km",
-          "type": "symbol",
-          "source": {
-            "url": "mapbox://giosans.636mnlg8",
-            "type": "vector"
-          },
-          "source-layer": "Doorgaande_NAP_-20_m_lijn__2_km",
-          "minzoom": 4,
-          "layout": {
-            "text-field": "{Name}",
-            "symbol-placement": "line",
-            "text-size": 16
-          },
-          "paint": {
-            "text-color": "white",
-            "text-halo-color": "black",
-            "text-halo-width": 2,
-            "text-halo-blur": 1
-          },
-
-        }
-      ],
-      sources: [
-      ]
+      toggle: '',
+      layers: [],
+      sources: []
     };
   },
   mounted() {
@@ -112,9 +47,7 @@ export default {
     let jarkusloaded = false;
 
     this.$refs.map.map.on('load', () => {
-      _.each(this.layers, (layer) => {
-        this.$refs.map.map.addLayer(layer)
-      })
+      this.addLayers();
       // we can only add these layers after fetching the mapid and token
       fetch("http://coastal-test.eu-west-1.elasticbeanstalk.com/vaklodingen")
         .then(resp => {
@@ -126,6 +59,7 @@ export default {
             id: "imageLayer",
             name: "vaklodingen",
             type: "raster",
+            // active: false,
             source: {
               type: "raster",
               tiles: [mapUrl],
@@ -146,6 +80,7 @@ export default {
             id: "Jarkus",
             name: "Jarkus",
             type: "line",
+            // active: false,
             source: {
               type: "geojson",
               data: json
@@ -162,7 +97,7 @@ export default {
           this.$refs.map.map.on('click', 'Jarkus', (e) => {
             var id = e.features[0].id
             // TODO: embed those pages here
-            window.open('http://coastal-test.eu-west-1.elasticbeanstalk.com/coastviewer/1.1.0/transects/' + id.toString() + '/info','_blank');
+            window.open('http://coastal-test.eu-west-1.elasticbeanstalk.com/coastviewer/1.1.0/transects/' + id.toString() + '/info', '_blank');
 
           });
 
@@ -174,38 +109,36 @@ export default {
           this.$refs.map.map.on('mouseenter', 'Jarkus', (e) => {
             this.$refs.map.map.getCanvas().style.cursor = 'pointer';
             popup.setLngLat([e.lngLat.lng, e.lngLat.lat])
-              .setHTML("Transect Id: " +  e.features[0].id.toString())
+              .setHTML("Transect Id: " + e.features[0].id.toString())
               .addTo(this.$refs.map.map);
           });
           this.$refs.map.map.on('mouseleave', 'Jarkus', () => {
             this.$refs.map.map.getCanvas().style.cursor = '';
             popup.remove();
           });
-
-
         });
       this.$refs.map.map.on('zoomend', (e) => {
         if (this.$refs.map.map.getZoom() >= 14) {
           this.$refs.map.map.setFilter('Jarkus', ['<=', 'lod', 64]);
-        }
-        else if (this.$refs.map.map.getZoom() >= 11) {
+        } else if (this.$refs.map.map.getZoom() >= 11) {
           this.$refs.map.map.setFilter('Jarkus', ['<=', 'lod', 32]);
-        }
-        else if (this.$refs.map.map.getZoom() >= 8) {
+        } else if (this.$refs.map.map.getZoom() >= 8) {
           this.$refs.map.map.setFilter('Jarkus', ['<=', 'lod', 16]);
-        }
-        else if (this.$refs.map.map.getZoom() >= 5) {
+        } else if (this.$refs.map.map.getZoom() >= 5) {
           this.$refs.map.map.setFilter('Jarkus', ['<=', 'lod', 8]);
         }
       });
-
-
     });
     bus.$emit('select-layers', this.layers);
-
-
-
-
+  },
+  watch: {
+    "layers": {
+      handler: function(layers) {
+        console.log(layers)
+        this.toggleLayers();
+      },
+      deep: true
+    }
   },
   methods: {
     getTileUrl(mapId, token) {
@@ -216,6 +149,48 @@ export default {
     },
     addDeck() {
 
+    },
+
+    addLayers() {
+      fetch("./static/data/datalayers.json")
+        .then(resp => {
+          return resp.json();
+        })
+        .then(json => {
+          _.each(json, (layer) => {
+            if (layer.type === 'group') {
+              _.each(layer.data, (sublayer) => {
+                this.$refs.map.map.addLayer(sublayer)
+              })
+            } else {
+              this.$refs.map.map.addLayer(layer)
+            }
+            this.layers.push(layer);
+          })
+        })
+    },
+
+    toggleLayers() {
+      // TODO: This could be a bit more efficient
+      _.each(this.layers, (layer) => {
+        if (layer.active) {
+          if (layer.type === 'group') {
+            _.each(layer.data, (sublayer) => {
+              this.$refs.map.map.setLayoutProperty(sublayer.id, "visibility", "visible");
+            })
+          } else {
+            this.$refs.map.map.setLayoutProperty(layer.id, "visibility", "visible");
+          }
+        } else {
+          if (layer.type === 'group') {
+            _.each(layer.data, (sublayer) => {
+              this.$refs.map.map.setLayoutProperty(sublayer.id, "visibility", "none");
+            });
+          } else {
+            this.$refs.map.map.setLayoutProperty(layer.id, "visibility", "none");
+          }
+        }
+      });
     }
   }
 };
