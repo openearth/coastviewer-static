813287a768e6841e326d5e7cd675b7c49f1401b1
CindyvdVries <cindyvdvries@gmail.com>
2018-06-21 16:18:17 +0200

    working deck-gl test layer

--- src/components/data-layers.js
+++ src/components/data-layers.js
@@ -1,18 +1,10 @@
 import {
-  experimental,
-  GeoJsonLayer
-} from 'deck.gl';
-import {
   bus
 } from '@/event-bus.js';
-const {
-  DeckGLJS,
-  MapControllerJS
-} = experimental;
+
 import Vue2MapboxGL from 'vue2mapbox-gl';
 import Vue from 'vue';
 import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 Vue.use(Vue2MapboxGL);
 
@@ -23,30 +15,31 @@ export default {
   data() {
     return {
       layers: [],
-      sources: []
+      sources: [],
+      map: null
     };
   },
   mounted() {
-    this.$refs.map.$on('mb-load', (event) => {
-      bus.$emit('map-loaded', event)
-    });
-    let viewport = {
-      latitude: 52,
+    let deckgl = new DeckGL({
+      container: 'deck-gl',
+      mapboxApiAccessToken: 'pk.eyJ1Ijoic2lnZ3lmIiwiYSI6Il8xOGdYdlEifQ.3-JZpqwUa3hydjAJFXIlMA',
+      mapStyle: 'mapbox://styles/mapbox/satellite-streets-v10',
       longitude: 4,
-      zoom: 10,
-      bearing: 0,
-      pitch: 60
-    };
-    let deckgl = new DeckGLJS({
-      // TODO EventManager should accept element id
-      /* global document */
-      canvas: document.getElementById('deck'),
-      ...viewport,
-      width: 300,
-      height: 300,
-      debug: true
-    });
-    this.$refs.map.$on('mb-load', (event) => {
+      latitude: 52,
+      zoom: 12,
+      layers: [
+        new ScatterplotLayer({
+          data: [
+            {position: [4, 52], color: [255, 0, 0], radius: 1000}
+          ],
+          opacity: 0.3
+        })
+      ]
+    })
+    console.log(deckgl)
+    this.map = deckgl.getMapboxMap()
+    console.log(this.map)
+    this.map.on('load', (event) => {
       // map is loaded, notify everyone
       bus.$emit('map-loaded', event);
       this.addLayers();
@@ -61,7 +54,7 @@ export default {
           let mapUrl = this.getTileUrl(json.mapid, json.token);
           let layer = this.layers.find(item => item.name === "Vaklodingen").data[0]
           layer.source.tiles = [mapUrl]
-          this.$refs.map.map.addLayer(layer);
+          this.map.addLayer(layer);
           bus.$emit('select-layers', this.layers);
         });
 
@@ -73,10 +66,10 @@ export default {
         .then(json => {
           let layer = this.layers.find(item => item.name === "Jarkus").data[0]
           layer.source.data = json
-          this.$refs.map.map.addLayer(layer);
+          this.map.addLayer(layer);
 
-          this.$refs.map.map.setFilter('jarkus', ['<=', 'lod', 16]);
-          this.$refs.map.map.on('click', 'jarkus', (e) => {
+          this.map.setFilter('jarkus', ['<=', 'lod', 16]);
+          this.map.on('click', 'jarkus', (e) => {
             let id = e.features[0].id;
             // TODO: embed those pages here
             window.open(coastviewerServer + '/coastviewer/1.1.0/transects/' + id.toString() + '/info','_blank');
@@ -89,39 +82,39 @@ export default {
           });
 
           // Mouse events
-          this.$refs.map.map.on('mouseenter', 'jarkus', (e) => {
-            this.$refs.map.map.getCanvas().style.cursor = 'pointer';
+          this.map.on('mouseenter', 'jarkus', (e) => {
+            this.map.getCanvas().style.cursor = 'pointer';
             popup.setLngLat([e.lngLat.lng, e.lngLat.lat])
               .setHTML("Transect Id: " + e.features[0].id.toString())
-              .addTo(this.$refs.map.map);
+              .addTo(this.map);
           });
-          this.$refs.map.map.on('mouseleave', 'jarkus', () => {
-            this.$refs.map.map.getCanvas().style.cursor = '';
+          this.map.on('mouseleave', 'jarkus', () => {
+            this.map.getCanvas().style.cursor = '';
             popup.remove();
           });
 
-          this.$refs.map.map.on("click", "dijkringpolygonen", (e) => {
+          this.map.on("click", "dijkringpolygonen", (e) => {
             popup.setLngLat([e.lngLat.lng, e.lngLat.lat])
               .setHTML(e.features[0].properties.description)
-              .addTo(this.$refs.map.map);
+              .addTo(this.map);
           })
 
-          this.$refs.map.map.on("mousemove", "dijkringpolygonen", (e) => {
-            this.$refs.map.map.setFilter("dijkringlijnen", ["==", "name", e.features[0].properties.name]);
+          this.map.on("mousemove", "dijkringpolygonen", (e) => {
+            this.map.setFilter("dijkringlijnen", ["==", "name", e.features[0].properties.name]);
           })
 
-          this.$refs.map.map.on("mouseleave", "dijkringpolygonen", (e) => {
-            this.$refs.map.map.setFilter("dijkringlijnen", ["==", "name", ""]);
+          this.map.on("mouseleave", "dijkringpolygonen", (e) => {
+            this.map.setFilter("dijkringlijnen", ["==", "name", ""]);
           })
         });
 
         // Set Level of detail for zoomlevels
         const zoomlevels = [14, 11, 8, 5]
         const lod = [64, 32, 16, 8]
-        this.$refs.map.map.on('zoomend', (e) => {
+        this.map.on('zoomend', (e) => {
           _.each(zoomlevels, (zoomlevel, i) => {
-            if (this.$refs.map.map.getZoom() >= zoomlevel) {
-              this.$refs.map.map.setFilter('jarkus', ['<=', 'lod', lod[i]]);
+            if (this.map.getZoom() >= zoomlevel) {
+              this.map.setFilter('jarkus', ['<=', 'lod', lod[i]]);
             }
           })
         });
@@ -153,14 +146,16 @@ export default {
           _.each(json, (layer) => {
             if (layer.layertype ===  'mapbox-layer-group') {
               _.each(layer.data, (sublayer) => {
-                this.$refs.map.map.addLayer(sublayer)
+                this.map.addLayer(sublayer)
               });
             } else if (layer.layertype ===  'mapbox-layer') {
-              this.$refs.map.map.addLayer(layer)
+              this.map.addLayer(layer)
             }
             this.layers.push(layer);
           });
         });
     }
+  },
+  components: {
   }
 };
