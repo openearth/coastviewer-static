5a7a9318fc1849d6065c3ade90c3ed9a7867371f
CindyvdVries <cindyvdvries@gmail.com>
2018-12-04 17:06:05 +0100

    set initial timeExtent and solved colorbar in Firefox

--- src/components/LayerControl.vue
+++ src/components/LayerControl.vue
@@ -2,17 +2,22 @@
   <div class="layer-control">
     <draggable class="draggable" v-model="computedList" @start="drag=true" @end="drag=false">
       <v-list two-line dense pt-0 v-for="layer in layers" :key="layer.id">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-switch  v-model="layer.active"></v-switch>
-            </v-list-tile-action>
-            <v-list-tile-title> {{layer.name}}
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-switch  v-model="layer.active"></v-switch>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{layer.name}}
+            </v-list-tile-title>
+            <v-list-tile-sub-title>
               <div v-if="layer.barlegend" class="bar-wrapper">
                 <div :style="layer.barlegend" class="bar"></div>
                 <div class="bartext">{{layer.bartext}} </div>
               </div>
-            </v-list-tile-title>
-          </v-list-tile>
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </draggable>
   </div>
